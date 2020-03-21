71e7d75ebf12614986ae41e2c787eddc70c11a9b
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-21 04:49:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,8 @@ def changing_alan
       }
     }
     
-    programmer_hash[:alan_kay][:known_for] = "GUI"
+    alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
     programmer_hash
 
 end
